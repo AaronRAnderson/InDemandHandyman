80ea7ff6b0098098fc67f4cc0adc0633b3a39de0
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-10-24 10:59:35 -0600

    basic styles for footer and fixed some upgrade issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-end
+  before_action :set_copyright
+
+    def set_copyright
+      @copyright = BigMouthFrogCopyrightTool::Renderer.copyright("Big Mouth Frog Development", "All rights reserved")
+    end
+  end
+
+  module BigMouthFrogCopyrightTool
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | Powered by: <a href='bigmouthfrogdev.com'>#{name}</a> | #{msg}".html_safe
+      end
+    end
+  end
